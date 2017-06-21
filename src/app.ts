b03d2376260d483f2eb532081d5a011d27bf9a57
Dan Ford <Dan@Fordsville.co.uk>
2017-06-21 15:06:26 +0100

    fixed some rubbish, got rid of some rubbish, you know how it is

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,10 @@
-import { auth, client, input } from "./common/common"
+import { auth, client } from "./common/common"
 
-input.on("line", (input: string) => {
-    ServerCommands.process(input)
-})
 
 client.on("ready", () => {
-    console.log(`${prefix.discord} Ready as ${client.user.username}`)
+    console.log(`Ready as ${client.user.username}`)
 })
 
 client.login(auth.token)
-console.log(`${prefix.discord} Logged in`)
+console.log(`Logged in`)
 
